34bc7e90ae269f52c1d10e3c205d5e0b364e63c4
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-04 10:45:24 +0530

    Added base code

---
+++ atm.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+	// function prototypes
+void printIntroMenu();
+void printMainMenu();
+void start();
+void login();
+void createAccount();
+
+// global variable (use this variable to store the user’s menu selection)
+char menuInput;
+
+// the main function
+int main()
+{
+	// TO WRITE A WELCOME MESSAGE HERE
+    
+		// call the function start
+		start();
+
+		return 0;
+}
+
+void printIntroMenu()
+{
+	// WRITE CODE HERE
+}
+
+void printMainMenu()
+{
+	// WRITE CODE HERE
+}
+
+void start()
+{
+	// EXPLANATION OF CODE THAT GOES HERE IS BELOW
+}
+
+void createAccount()
+{
+	// PHASE 2
+}
+
+void login()
+{
+	// PHASE 2
+}
